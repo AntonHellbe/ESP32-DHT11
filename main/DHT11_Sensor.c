0e3e94261cfbb1af59fa486e9b792aaf0fb613f6
AntonHellbe <antonhellbe@gmail.com>
2018-10-08 22:05:32 +0200

    Small changes

--- main/DHT11_Sensor.c
+++ main/DHT11_Sensor.c
@@ -7,12 +7,15 @@
 #define DHT_DATA_LENGTH     40
 #define DHT_DATA_PIN_HIGH   1
 #define DHT_DATA_PIN_LOW    0
+#define SEND_START_DELAY_1  22000
+#define SEND_START_DELAY_2  43
+#define DHT_TIMEOUT_LIMIT   200
 #define IS_BIT_HIGH(X)      (X > 40 ? (true) : (false))
 
 
 static void dht__check_length(uint8_t * p_bit_count, uint8_t * p_byte_count);
 static dht_ret_code_t dht__data_transmission_seq(dht_sensor_t * p_dht_sensor);
-static dht_ret_code_t dht__expect_pulse(uint8_t timeout_limit, uint8_t pin_level, uint8_t * p_delay_counter)
+static dht_ret_code_t dht__expect_pulse(uint8_t pin_level, uint8_t * p_delay_counter);
 static uint8_t dht__data_level(void);
 static void dht__send_start(void);
 
@@ -33,9 +36,9 @@ static void dht__send_start(void)
 {
     gpio_set_direction(dht_data_pin, GPIO_MODE_OUTPUT);
     gpio_set_level(dht_data_pin, DHT_DATA_PIN_LOW);
-    ets_delay_us(22000);
+    ets_delay_us(SEND_START_DELAY_1);
     gpio_set_level(dht_data_pin, DHT_DATA_PIN_HIGH);
-    ets_delay_us(43);
+    ets_delay_us(SEND_START_DELAY_2);
     gpio_set_direction(dht_data_pin, GPIO_MODE_INPUT);
 }
 
@@ -45,7 +48,7 @@ dht_ret_code_t dht__get_sensor_values(dht_sensor_t * p_dht_sensor)
     uint8_t delay_counter = 0;
 
     //Wait for a response from the DHT11 Sensor, waiting time is usually between 20-40 us
-    if(dht__expect_pulse(40, DHT_DATA_PIN_HIGH, &delay_counter) == DHT_TIMEOUT_ERR) 
+    if(dht__expect_pulse(DHT_DATA_PIN_HIGH, &delay_counter) == DHT_TIMEOUT_ERR) 
     {
         printf("DHT: Timeout error, No response from DHT11 \r\n");
         return DHT_TIMEOUT_ERR;
@@ -54,7 +57,7 @@ dht_ret_code_t dht__get_sensor_values(dht_sensor_t * p_dht_sensor)
     delay_counter = 0;
     // DHT has pulled data line low, it will be kept low for 80us and then high for another 80us
     // Check so DHT keeps the line low
-    if(dht__expect_pulse(80, DHT_DATA_PIN_LOW, &delay_counter) == DHT_TIMEOUT_ERR) 
+    if(dht__expect_pulse(DHT_DATA_PIN_LOW, &delay_counter) == DHT_TIMEOUT_ERR) 
     {
         printf("DHT: Timeout error, DHT11 has kept line low for more than 80us \r\n");
         return DHT_TIMEOUT_ERR;
@@ -62,7 +65,7 @@ dht_ret_code_t dht__get_sensor_values(dht_sensor_t * p_dht_sensor)
 
     delay_counter = 0;
     // Check that DHT11 keeps the line high
-    if(dht__expect_pulse(80, DHT_DATA_PIN_HIGH, &delay_counter) == DHT_TIMEOUT_ERR) 
+    if(dht__expect_pulse(DHT_DATA_PIN_HIGH, &delay_counter) == DHT_TIMEOUT_ERR) 
     {
         printf("DHT: Timeout error, DHT11 has kept line high for more than 80us \r\n");
         return DHT_TIMEOUT_ERR;
@@ -82,12 +85,13 @@ static dht_ret_code_t dht__data_transmission_seq(dht_sensor_t * p_dht_sensor)
     uint8_t delay_counter = 0;
     uint8_t i = 0;
 
+
     // Start of reading the actual sensor data
     // Data transmission size is 40 bits, i.e 5 bytes
     for(i = 0; i < DHT_DATA_LENGTH; i++)
     {
         // Data transmission starts with 50us low signal
-        if(dht__expect_pulse(55, DHT_DATA_PIN_LOW, &delay_counter) == DHT_TIMEOUT_ERR) 
+        if(dht__expect_pulse(DHT_DATA_PIN_LOW, &delay_counter) == DHT_TIMEOUT_ERR) 
         {
             printf("DHT: Timeout error, data transmission start signal more than 50us \r\n");
         }
@@ -95,7 +99,7 @@ static dht_ret_code_t dht__data_transmission_seq(dht_sensor_t * p_dht_sensor)
         delay_counter = 0;
 
         // Time to check if data is a 0 or 1
-        if(dht__expect_pulse(75, DHT_DATA_PIN_HIGH, &delay_counter) == DHT_TIMEOUT_ERR)
+        if(dht__expect_pulse(DHT_DATA_PIN_HIGH, &delay_counter) == DHT_TIMEOUT_ERR)
         {
             printf("DHT: Timeout error, could not read bit properly from DHT11 \r\n");
             return DHT_TIMEOUT_ERR;
@@ -131,11 +135,11 @@ static dht_ret_code_t dht__data_transmission_seq(dht_sensor_t * p_dht_sensor)
 
 }
 
-static dht_ret_code_t dht__expect_pulse(uint8_t timeout_limit, uint8_t pin_level, uint8_t * p_delay_counter)
+static dht_ret_code_t dht__expect_pulse(uint8_t pin_level, uint8_t * p_delay_counter)
 {
     while(dht__data_level() == pin_level)
     {
-        if(*p_delay_counter > timeout_limit)
+        if(*p_delay_counter > DHT_TIMEOUT_LIMIT)
         {
             printf("DHT: Timeout error \r\n");
             return DHT_TIMEOUT_ERR;
